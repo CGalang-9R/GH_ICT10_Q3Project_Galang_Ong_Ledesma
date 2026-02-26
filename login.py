c41c8a77c319b49c8cc93b0e41386521e9a56028
liana <galang.chm@obmontessori.edu.ph>
2026-02-26 19:01:03 +0800

    Update login.py

--- login.py
+++ login.py
@@ -5,11 +5,12 @@ def signup_complete(e):
     username = document.querySelector('input[name="username"]').value
     email = document.querySelector('input[name="email"]').value
     password = document.querySelector('input[name="password"]').value
-    if username.strip() and email.strip() and "@" in email and password.strip() and password > 7:
-        document.getElementById("output-login").innerHTML = ''
-        display(f'''Is this information correct?
-                Username: {username}, Email: {email}
-                <input type="button" py-click="signup_confirm" value="Login">''', target='output-login')
+    if username.strip() and email.strip() and "@" in email and password.strip()
+        if password > 7:
+            document.getElementById("output-login").innerHTML = ''
+            display(f'''Is this information correct?
+                    Username: {username}, Email: {email}
+                    <input type="button" py-click="signup_confirm" value="Login">''', target='output-login')
     else:
         document.getElementById("output-login").innerHTML = ''
         display(f'''Please fill in all required fields correctly.
