8ec2794e228f97107f07ca748c7637d20d474b78
liana <galang.chm@obmontessori.edu.ph>
2026-02-27 08:10:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def intrams(e):
         display(f'Please get a medical clearance from the clinic to be able to join the Intramurals.', target='output')
 
     elif gr > 10 or gr < 7:
-        display(f'Please enter JHS grade levels only.', target='output')
+        display(f'Please enter JHS grade levels ONLY.', target='output')
     
     elif sct == 'RUBY':
         display(f'Congratulations! You are part of the Blue Bears!', target='output')
